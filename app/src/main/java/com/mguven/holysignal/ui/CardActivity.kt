f3b0f378097e9117c9b074c88803e3d4c03dcc39
Mustafa Guven <mustafa.guven@mavi.com>
2019-10-07 17:21:00 +0300

    add: language

--- app/src/main/java/com/mguven/holysignal/ui/CardActivity.kt
+++ app/src/main/java/com/mguven/holysignal/ui/CardActivity.kt
@@ -142,59 +142,5 @@ class CardActivity : AbstractBaseActivity() {
     })
   }
 
-  /*private fun getAyahList(randomAyah: Int) {
-    holyBookViewModel.getAyahList().observe(this, Observer<List<AyahSampleData>> { list ->
-      Log.e("AAA", "==============> list.size ${list.size}")
-      list.forEach {
-        Log.e("AAA", "==============> ${it.Id} == ${it.text}")
-      }
-    })
-  }
-
-  private fun getSurahList() {
-    holyBookViewModel.getSurahList().observe(this, Observer<List<SurahData>> { list ->
-      Log.e("AAA", "==============> list.size ${list.size}")
-      list.forEach {
-        Log.e("AAA", "==============> ${it.Id} == ${it.englishName}")
-      }
-    })
-  }
-
-  private fun getEditionList() {
-    ApplicationDatabase(this).editionDataDao().getAll().observe(this, Observer<List<EditionData>> { list ->
-      Log.e("AAA", "==============> list.size ${list.size}")
-      list.forEach {
-        Log.e("AAA", "==============> ${it.Id} == ${it.identifier}")
-      }
-    })
-  }
-
-  private fun deleteAll() = runBlocking {
-    launch(Dispatchers.Default) {
-      Log.e("AAA", "==============> DELETE ${Thread.currentThread().name}")
-      ApplicationDatabase(this@CardActivity).editionDataDao().deleteAll()
-    }
-  }
-
-    private fun insert() = runBlocking {
-    launch(Dispatchers.Default) {
-      for (x in 200..1000) {
-        Log.e("AAA", "==============> INSERT ${Thread.currentThread().name}")
-        ApplicationDatabase(this@CardActivity).editionDataDao().insert(EditionData(x, "d", "tr", "name", "english name here", "text", "translation"))
-      }
-    }
-  }
-
-  private fun getFavourites() {
-    holyBookViewModel.getFavourites().observe(this, Observer<List<FavouritesData>> { list ->
-      Log.e("AAA", "favourites ==============> list.size ${list.size}")
-      list.forEach {
-        Log.e("AAA", "favourites ==============> ${it.Id} == ${it.ayahNumber}")
-      }
-    })
-  }
-  */
-
-
 }
 
