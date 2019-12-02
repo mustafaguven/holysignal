f0ca7e8fc37652e322b69e5fd77a4ec52c616c29
Mustafa Guven <mustafa.guven@mavi.com>
2019-12-02 09:40:41 +0300

    amend

--- app/src/main/java/com/mguven/holysignal/db/dao/SurahDataDao.kt
+++ app/src/main/java/com/mguven/holysignal/db/dao/SurahDataDao.kt
@@ -10,22 +10,25 @@ import com.mguven.holysignal.db.entity.SurahData
 @Dao
 interface SurahDataDao {
 
+  @Query("Select Case WHEN Sub.name IS NULL THEN Sub.englishName else Sub.name end as 'key', A.surahNumber as value, Sub.startingAyahNumber as 'min', Sub.endingAyahNumber as 'max' from AyahSample A Inner Join ( " +
+      " Select  S.number, S.englishName, T.name, S.startingAyahNumber, S.endingAyahNumber from Surah S " +
+      "       LEft join SurahTranslate T on S.number = T.surahNumber " +
+      "   and languageId = (Select L.Id from Language L Inner Join Edition E on L.abbreviation = E.language Where E.Id = :editionId) " +
+      "       group by S.englishName " +
+      "       order by S.number " +
+      " ) as Sub on Sub.number = A.surahNumber " +
+      " group by A.surahNumber " +
+      " order by A.surahNumber ")
+  suspend fun getAvailableSurahListByEditionId(editionId: Int): List<AvailableSurahItem>
+
   @Query("SELECT * from Surah")
   suspend fun getAll(): List<SurahData>
-
-  @Query("SELECT '(' || S.number || ')'  || ' ' ||  S.englishName as 'key', S.number as value, S.startingAyahNumber as 'min', S.endingAyahNumber as 'max' " +
-      " FROM Surah S INNER JOIN AyahSample A on S.number = A.surahNumber " +
-      " WHERE editionId = :editionId " +
-      " group by S.englishName " +
-      " order by S.Id ")
-  suspend fun getAvailableSurahListByEditionId(editionId: Int): List<AvailableSurahItem>
 
   @Insert(onConflict = REPLACE)
   suspend fun insert(surahData: SurahData)
 
   @Query("DELETE from Surah")
   suspend fun deleteAll()
-
 
 
 }
